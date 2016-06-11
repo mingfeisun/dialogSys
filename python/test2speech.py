abd17263da54b20bbff14f8b3c7d1145a5070ea6
MingfeiSUN <mingfei.sun.hk@gmail.com>
2016-06-11 17:32:13 +0800

    init

---
+++ python/test2speech.py
@@ -0,0 +1,18 @@
+# coding=utf8
+import sys
+from naoqi import ALProxy
+
+ip = "192.168.1.102"
+port = 9559
+
+try:
+    tts = ALProxy("ALTextToSpeech", ip, port)
+except Exception, e:
+    print "Error:", e
+    sys.exit(1)
+
+testString = "你好，我是人机交互实验室的新成员"
+
+tts.setLanguage("Chinese")
+tts.say(testString)
+tts.sayToFile(testString, "/home/nao/speechToSay/hello.wav")
