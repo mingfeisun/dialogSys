eb6933cf378d914b1df92be6ab097943678fda12
MingfeiSUN <mingfei.sun.hk@gmail.com>
2016-07-10 19:54:25 +0800

    modify class Converter

--- local/Converter/complieTo32bit.sh
+++ local/Converter/complieTo32bit.sh
@@ -1,6 +1,10 @@
-export CXXFLAGS=-m32
-export CFLAGS=-m32
-export LDFLAGS=-m32
+export CXXFLAGS='-m32'
+export CFLAGS='-m32'
+export LDFLAGS='-m32 -l:/lib32/libdl.so.2'
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib32
+export LIBS='-ldl'
 qibuild clean -f
 qibuild configure
 qibuild make
+# echo $LIBRARY_PATH
+# echo $LD_LIBRARY_PATH
