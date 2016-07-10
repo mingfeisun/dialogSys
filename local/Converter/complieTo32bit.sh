585d329133b24f28d9a0c2df24da5fabc4fbd314
MingfeiSUN <mingfei.sun.hk@gmail.com>
2016-07-10 19:55:39 +0800

    modify complieTo32bit

--- local/Converter/complieTo32bit.sh
+++ local/Converter/complieTo32bit.sh
@@ -1,8 +1,8 @@
-export CXXFLAGS='-m32'
-export CFLAGS='-m32'
-export LDFLAGS='-m32 -l:/lib32/libdl.so.2'
+# export CXXFLAGS='-m32'
+# export CFLAGS='-m32'
+# export LDFLAGS='-m32 -l:/lib32/libdl.so.2'
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib32
-export LIBS='-ldl'
+# export LIBS='-ldl'
 qibuild clean -f
 qibuild configure
 qibuild make
